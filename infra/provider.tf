a53d635c4a0ce9e9b6fad1fd671400a094f7ca97
Godwin AMEGAH <komlan.godwin.amegah@gmail.com>
2024-10-27 18:06:37 +0100

    backend

--- infra/provider.tf
+++ infra/provider.tf
@@ -1,4 +1,12 @@
 terraform {
+  // backend configuration for storing the state file
+    backend "s3" {
+        bucket = "spark-handson-tf-state-bucket "
+        key    = "tf-infra/terraform.tfstate"
+        region = "us-east-1"
+        dynamodb_table = "spark_handson_db_tf_state_lock"
+        encrypt = true
+    }
   required_providers {
     aws = {
       source  = "hashicorp/aws"
